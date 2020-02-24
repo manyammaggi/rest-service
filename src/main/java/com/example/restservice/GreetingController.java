3aa55cf3a9f1dada3f23ccef239ee75cf64abb19
manyammaggi <manyammaggi@gmail.com>
2020-02-24 11:43:20 -0600

    commit restservice

---
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -0,0 +1,22 @@
+//package com.example.restservice;
+
+//import java.util.concurrent.atomic.AtomicLong;
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//@RequestMapping("/greeting")
+//public class GreetingController {
+
+//	@RequestMapping(value="/{name}", method= RequestMethod.GET)
+	//public String sayHello(@PathVariable String name) {
+	
+	//	return "hello" + name;
+	//}	
+//}
